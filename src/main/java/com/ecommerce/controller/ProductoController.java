1e278c49e9dae0713ae0fe91239513eba87ad966
Fede <federicobrid@gmail.com>
2023-03-07 17:12:38 -0300

    crud productos terminado

--- src/main/java/com/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/controller/ProductoController.java
@@ -1,11 +1,14 @@
 package com.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -29,6 +32,25 @@ public class ProductoController {
 	@GetMapping("/create")
 	public String create() {
 		return "productos/create";
+	}
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Producto producto = new Producto();
+		Optional<Producto> optionalProducto = productoService.get(id);
+		producto = optionalProducto.get();
+		model.addAttribute("producto", producto);
+		LOGGER.info("producto buscado por id {}",producto);
+		return "productos/edit";
+	}
+	@PostMapping("/update")
+	public String update(Producto producto) {
+		productoService.update(producto);
+		return "redirect:/productos";
+	}
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+		productoService.delete(id);
+		return "redirect:/productos";
 	}
 	@PostMapping("/save")
 	public String save(Producto producto) {
