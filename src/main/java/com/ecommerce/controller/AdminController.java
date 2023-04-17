038985a3715a3f254cb6e6d4c2c4e770e5063f56
Fede <federicobrid@gmail.com>
2023-04-16 23:38:58 -0300

    ver detalle ordenes admin

--- src/main/java/com/ecommerce/controller/AdminController.java
+++ src/main/java/com/ecommerce/controller/AdminController.java
@@ -2,12 +2,16 @@ package com.ecommerce.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ecommerce.model.Orden;
 import com.ecommerce.model.Producto;
 import com.ecommerce.model.Usuario;
 import com.ecommerce.service.IOrdenService;
@@ -25,6 +29,8 @@ public class AdminController {
 	
 	@Autowired
 	private IOrdenService ordenService;
+	
+	private Logger LOGGER = LoggerFactory.getLogger(AdminController.class);
 
 	@GetMapping("")
 	public String home(Model model) {
@@ -42,5 +48,12 @@ public class AdminController {
 	public String ordenes(Model model) {
 		model.addAttribute("ordenes", ordenService.findAll());
 		return "admin/ordenes";
+	}
+	@GetMapping("/detalle/{id}")
+	public String detalle(@PathVariable Integer id, Model model) {
+		LOGGER.info("detalle orden por id: {}",id);
+		Orden orden = ordenService.findById(id).get();
+		model.addAttribute("detalles",orden.getDetalle());
+		return "admin/detalleorden";
 	}
 }
